79095cbbf228a5ca3855ee70fc72fe27bcea8f48
yoga2172 <yoganandh999@gmail.com>
2021-01-21 00:04:23 +0530

    front end

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Navbar from "./components/navbar.component";
+
+
+class App extends Component {
+   render() {   
+    return (
+     <div>
+       <Navbar />
+      
+     </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/employee/board-user.component.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+import UserService from "../../services/user.service";
+import Employee from "./employeecomponent";
+
+
+
+export default class BoardUser extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: ""
+    };
+  }
+
+  componentDidMount() {
+    UserService.getUserBoard().then(
+      response => {
+        this.setState({
+          content: response.data
+        });
+      },
+      error => {
+        this.setState({
+          content:
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString()
+        });
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <Employee />       
+      </div>
+    );
+  }
+}
